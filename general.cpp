e24a1bf0688ecc99658005fa4956c4190df92688
vartika4160 <139868043+vartika4160@users.noreply.github.com>
2023-10-21 23:30:13 +0530

    Create general.cpp

---
+++ general.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+//Base class
+class Vehicle
+{
+    public:
+    string brand="Ford_";
+    void color()
+    {
+        cout<<"Red!\n";
+    }
+};
+//Derived class
+class Car:public Vehicle
+{
+    public:
+    string model="Mustang";
+};
+int main()
+{
+    Car myCar;
+    myCar.color();
+    cout<<myCar.brand+""+myCar.model;
+    return 0;
+}
+/*Output
+Red!
+Ford_Mustang
+*/
